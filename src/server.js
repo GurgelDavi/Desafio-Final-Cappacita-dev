515c4279dc70bee632caf0ccf35fbb26f3b590c6
Davi <davigurgel@virtual.ufc.br>
2021-09-18 13:27:16 -0300

    remove console.log

--- src/server.js
+++ src/server.js
@@ -8,12 +8,10 @@ app.use(bodyParser.urlencoded({extended : true }))
 
 app.get('/movies', async (req, res) => {
     const movies = await gettingMovies.getMoviesPlaying();
-    console.log(movies);
     res.send(movies)
 }) 
 app.get('/movies/:id', async (req, res) => {
     const movies = await gettingMovies.getMovie(req.params.id);
-    console.log(movies);
     res.send(movies)
 }) 
 
