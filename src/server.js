7ced268baf9961f613fd1e213db4e6db11d93e81
Davi <davigurgel@virtual.ufc.br>
2021-09-19 21:10:19 -0300

    update status code

--- src/server.js
+++ src/server.js
@@ -7,27 +7,48 @@ const gettingMovies = require('../moviesData/getting movies')
 app.use(bodyParser.urlencoded({extended : true }))
 
 app.get('/movies', async (req, res) => {
-    const movies = await gettingMovies.getMoviesPlaying();
-    res.send(movies)
+    try {
+        const movies = await gettingMovies.getMoviesPlaying();
+        return res.status(200).send(movies);
+    } catch (error) {
+        return error
+    }
 })
 
 app.get('/movies/:query', async (req, res) => {
-    const movies = await gettingMovies.searchMovie(req.params.query);
-    res.send(movies)
+    try {
+        const movies = await gettingMovies.searchMovie(req.params.query);
+        return res.status(200).send(movies);
+    } catch (error) {
+        return error
+    }
 })
 
 app.get('/movie/:id', async (req, res) => {
-    const movies = await gettingMovies.getMovie(req.params.id);
-    res.send(movies)
+    try {
+        const movies = await gettingMovies.getMovie(req.params.id);
+        return res.status(200).send(movies);   
+    } catch (error) {
+        return error
+    }
+    
 })
 app.post('/movies/:query', async (req, res) => {
-    const movies = await gettingMovies.trackActivity(req.params.query);
-    res.send(movies)
+    try {
+        const movies = await gettingMovies.trackActivity(req.params.query);
+        return res.status(201).send(movies);
+    } catch (error) {
+        return error
+    }
 })
 
 app.post('/movie/:id', async (req, res) => {
-    const movies = await gettingMovies.trackClick(req.params.id);
-    res.send(movies)
+    try {
+        const movies = await gettingMovies.trackClick(req.params.id);
+        return res.status(201).send(movies);
+    } catch (error) {
+        return error
+    }
 })
 
 app.listen(3003)
